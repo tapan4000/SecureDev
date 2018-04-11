bc8f3a0e589d57dc58c8c57fd60ec6bb5e65a698
tapan4000 <tapan8000@gmail.com>
2018-04-11 07:10:25 -0400

    Modified the audio project for some audio improvements.

--- Sketches/Arduino_POC/BlankSketch/BlankSketch.ino
+++ Sketches/Arduino_POC/BlankSketch/BlankSketch.ino
@@ -4,7 +4,22 @@
 // Determine if the 
 
 void setup() {
-	
+	Serial.begin(9600);
+	short sampleData = -450;
+	byte lowByte = sampleData;
+	Serial.println(lowByte);
+	byte highByte = sampleData >> 8;
+	Serial.println(highByte);
+	byte testByte = sampleData << 8;
+	Serial.println(testByte);
+
+	short sampleData2 = 450;
+	byte lowByte2 = sampleData2;
+	Serial.println(lowByte2);
+	byte highByte2 = sampleData2 >> 8;
+	Serial.println(highByte2);
+	byte testByte2 = sampleData2 << 8;
+	Serial.println(testByte2);
 }
 
 void loop() {
