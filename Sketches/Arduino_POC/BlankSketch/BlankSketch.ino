352e705264662d67ee9e1af66ec1a9b273a64b0d
tapan4000 <tapan8000@gmail.com>
2018-04-23 06:28:29 -0400

    Improved the audio quality

--- Sketches/Arduino_POC/BlankSketch/BlankSketch.ino
+++ Sketches/Arduino_POC/BlankSketch/BlankSketch.ino
@@ -2,26 +2,60 @@
 //SoftwareSerial moduleSerial(0, 1); //RX, TX
 
 // Determine if the 
-
+int counter = 0;
+unsigned long maxMicros = 0;
+unsigned long lastMacros = 0;
+unsigned long minMicros = 99999;
+unsigned long sample = 0;
 void setup() {
-	Serial.begin(9600);
+	Serial.begin(115200);
+	Serial.println("Print -450");
 	short sampleData = -450;
 	byte lowByte = sampleData;
 	Serial.println(lowByte);
 	byte highByte = sampleData >> 8;
 	Serial.println(highByte);
-	byte testByte = sampleData << 8;
-	Serial.println(testByte);
+	short formedSample = lowByte | highByte << 8;
+	Serial.println(formedSample);
 
+	Serial.println("Print 450");
 	short sampleData2 = 450;
 	byte lowByte2 = sampleData2;
 	Serial.println(lowByte2);
 	byte highByte2 = sampleData2 >> 8;
 	Serial.println(highByte2);
-	byte testByte2 = sampleData2 << 8;
-	Serial.println(testByte2);
+
+	Serial.println("Print 450 by forming a number reading the bytes from it.");
+	short sampleData3 = lowByte2 | highByte2 << 8;
+	Serial.println(sampleData3);
+	byte lowByte3 = sampleData3;
+	Serial.println(lowByte3);
+	byte highByte3 = sampleData3 >> 8;
+	Serial.println(highByte3);
 }
 
 void loop() {
- 
+	if (counter < 10005) {
+		counter++;
+	}
+	
+	if (counter == 1) {
+		Serial.println("Start");
+		lastMacros = micros();
+	}
+	else if (counter < 10000) {
+		sample = micros() - lastMacros;
+		lastMacros = micros();
+		if (maxMicros < sample) {
+			maxMicros = sample;
+		}
+		else if(minMicros > sample) {
+			minMicros = sample;
+		}
+	}
+	else if (counter == 10000) {
+		Serial.println("End");
+		Serial.println(minMicros);
+		Serial.println(maxMicros);
+	}
 }
